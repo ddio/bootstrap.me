957d37f859f1d4d0e9360026f26336f2d2e5063c
ddio <ddio.chuang@lileesystems.com>
2013-09-05 14:14:22 +0800

    add full pkt list, update vim script

--- pkt.sh
+++ pkt.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-apt-get install git
-apt-get install tree curl node npm 
+BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser"
+DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox compizconfig-settings-manager unity-tweak-tool"
+TOOLS="tree cscope ctags"
+C_DEVEL="autoconf g++ clang"
+WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql apache2-mpm-event"
+
+
+apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
 
 npm install -g npm
 
-
 mkdir ~/src
-
 mkdir ~/tmp
 
--- pkts/vim.sh
+++ pkts/vim.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle 
-curl 'www.vim.org/scripts/download_script.php?src_id=19375' > ~/.vim/autoload/pathogen.vim
-
+mkdir -p ~/.vim/autoload ~/.vim/bundle
 cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/vim-scripts/taglist.vim.git
 git clone https://github.com/plasticboy/vim-markdown.git
+
+(cd ~/.vim/autoload ; ln -s ../bundle/vim-pathogen/autoload/pathogen.vim)
 
 wget https://github.com/downloads/kchmck/vim-coffee-script/vim-coffee-script-v002.zip
 unzip -od ~/.vim vim-coffee-script-v002.zip
