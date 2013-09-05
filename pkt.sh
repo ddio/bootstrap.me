a39c6e96b56ab6cf2b57a1b545ab89118e72277a
ddio <ddio.chuang@lileesystems.com>
2013-09-05 16:48:33 +0800

    add email notifier

--- pkt.sh
+++ pkt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser"
-DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox compizconfig-settings-manager unity-tweak-tool"
+BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser subversion"
+DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox \
+       compizconfig-settings-manager unity-tweak-tool unity-notify"
 TOOLS="tree cscope ctags"
 C_DEVEL="autoconf g++ clang"
 WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql apache2-mpm-event"
@@ -11,6 +12,6 @@ apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
 
 npm install -g npm
 
-mkdir ~/src
-mkdir ~/tmp
+mkdir -p ~/src
+mkdir -p ~/tmp
 
