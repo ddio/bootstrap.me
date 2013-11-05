7a7db7831e62bc7a15a199d7d031466894368130
Ddio Chuang <ddio.chuang@lileesystems.com>
2013-11-05 17:39:00 +0800

    add php curl

--- pkt.sh
+++ pkt.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser subversion"
+BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser subversion \
+		openssh-server"
 DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox \
        compizconfig-settings-manager unity-tweak-tool unity-notify"
 TOOLS="tree cscope ctags"
 C_DEVEL="autoconf g++ clang"
-WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql apache2-mpm-event"
+WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql \
+	   apache2-mpm-event libapache2-mod-fastcgi"
 
 
 apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
