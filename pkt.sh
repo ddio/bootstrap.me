605d55ca734b9ab74286ca025596f12ba564d11d
Ddio Chuang <ddio.chuang@lileesystems.com>
2014-07-24 14:24:45 +0800

    use correct apache mode

--- pkt.sh
+++ pkt.sh
@@ -6,8 +6,8 @@ DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox \
        compizconfig-settings-manager unity-tweak-tool unity-notify"
 TOOLS="tree cscope ctags"
 C_DEVEL="autoconf g++ clang"
-WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql \
-	   apache2-mpm-event libapache2-mod-fastcgi"
+WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql php5-pgsql \
+	   apache2-mpm-worker libapache2-mod-fastcgi "
 
 
 apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
