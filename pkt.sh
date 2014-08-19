dd1c282d54062d3a9ab0accc7b71b881a6b9a7cd
Ddio Chuang <ddio.chuang@lileesystems.com>
2014-08-19 14:22:17 +0800

    add gnome3

--- pkt.sh
+++ pkt.sh
@@ -3,12 +3,12 @@
 BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser subversion \
 			openssh-server"
 DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox \
-       compizconfig-settings-manager unity-tweak-tool unity-notify"
+       compizconfig-settings-manager gnome-tweak-tool gnome-shell"
 TOOLS="tree cscope ctags"
 C_DEVEL="autoconf g++ clang"
 WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql php5-pgsql \
 	   apache2-mpm-worker libapache2-mod-fastcgi "
-
+WORKS="openconnect network-manager-openconnect-gnome"
 
 apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
 
