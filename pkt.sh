d10abdcce25babe5b58e7deafc9d70a345228f38
ddio <ddioibb@gmail.com>
2022-11-22 10:11:23 +0800

    chore: update pkg

--- pkt.sh
+++ pkt.sh
@@ -3,16 +3,19 @@
 BASIC="synaptic vim gitg virtualbox sqlite3 sqlitebrowser subversion \
 			openssh-server"
 DAILY="vlc smplayer libdbd-sqlite3 gnucash gimp nautilus-dropbox \
-       compizconfig-settings-manager gnome-tweak-tool gnome-shell"
+       compizconfig-settings-manager gnome-tweak-tool gnome-shell curl \
+       comix meld htop"
 TOOLS="tree cscope ctags"
-C_DEVEL="autoconf g++ clang"
-WEB_DEVEL="curl node npm php5-fpm php5-sqlite php5-gd php5-mysql php5-pgsql \
+DEV="autoconf g++ clang python3-pip"
+PHP_DEVEL="php5-fpm php5-sqlite php5-gd php5-mysql php5-pgsql \
 	   apache2-mpm-worker libapache2-mod-fastcgi "
 WORKS="openconnect network-manager-openconnect-gnome"
 
-apt-get install $BASIC $DAILY $TOOLS $C_DEVEL $WEB_DEVEL
+apt-get install $BASIC $DAILY $TOOLS $DEV
 
-npm install -g npm
+# install ibus
+
+# install nvm
 
 mkdir -p ~/src
 mkdir -p ~/tmp
